8c5e6149b30ef36b2e022a9c8479811e5f725ef2
my2248 <>
2010-05-26 06:18:14 +0000

    implement SingleThreadModel

--- src/org/geworkbench/components/interactions/cellularnetwork/InteractionsConnectionImplServlet.java
+++ src/org/geworkbench/components/interactions/cellularnetwork/InteractionsConnectionImplServlet.java
@@ -18,7 +18,8 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse; 
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.SingleThreadModel;
 
 import sun.misc.BASE64Decoder;
 
@@ -36,14 +37,14 @@ import sun.misc.BASE64Decoder;
  * @see ResultSetlUtil
  * 
  */
-public class InteractionsConnectionImplServlet extends HttpServlet {
+public class InteractionsConnectionImplServlet extends HttpServlet  implements SingleThreadModel {
 	private static final int MIN_DATA_LENGTH = 1;
 	private static final String GET_PAIRWISE_INTERACTION = "getPairWiseInteraction";
 	private static final String GET_INTERACTION_TYPES = "getInteractionTypes";
 	private static final String GET_DATASET_NAMES = "getDatasetNames";
 	private static final String GET_VERSION_DESCRIPTOR = "getVersionDescriptor";
 	private static final String CLOSE_DB_CONNECTION = "closeDbConnection";
-	
+
 	/**
 	 * Logger for this class
 	 */
@@ -76,7 +77,8 @@ public class InteractionsConnectionImplServlet extends HttpServlet {
 	private static String mysql_url;
 	private static String dataset_passwd;
 	private static String dataset_user;
-	private static Connection conn = null;
+	private Connection conn = null;
+	private PreparedStatement statement = null;
 	private static String message = "Error processing SQL query";
 
 	@Override
@@ -103,15 +105,13 @@ public class InteractionsConnectionImplServlet extends HttpServlet {
 					.trim();
 
 			Class.forName(mysql_jdbc_driver);
-		 
 
 		} catch (ClassNotFoundException e) {
 			logger
 					.error(
 							"init() - exception  in InteractionsConnectionImplServlet init  ---- ", e); //$NON-NLS-1$
 			logger.error("init()", e); //$NON-NLS-1$
-		}
-		catch (IOException ie) {
+		} catch (IOException ie) {
 			logger
 					.error(
 							"init() - exception  in InteractionsConnectionImplServlet init  ---- ", ie); //$NON-NLS-1$
@@ -127,14 +127,12 @@ public class InteractionsConnectionImplServlet extends HttpServlet {
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
-		
-		
+
 		String methodName = "";
 		boolean isSqlException = false;
-		
+
 		try {
 
-			
 			String db = null;
 
 			if (logger.isDebugEnabled()) {
@@ -168,8 +166,8 @@ public class InteractionsConnectionImplServlet extends HttpServlet {
 			String[] tokens = inString.split(REGEX_DEL, SPLIT_ALL);
 			db = tokens[0];
 			methodName = tokens[1].trim();
-			
-			if (methodName.equalsIgnoreCase(CLOSE_DB_CONNECTION))  
+
+			if (methodName.equalsIgnoreCase(CLOSE_DB_CONNECTION))
 				return;
 
 			if (db.equals(ORACLE)) {
@@ -182,30 +180,30 @@ public class InteractionsConnectionImplServlet extends HttpServlet {
 						.error(
 								"doPost(HttpServletRequest, HttpServletResponse) - This should never happens as Oracle is not used any more....", null); //$NON-NLS-1$
 			}
-		 
-            if ( conn == null ||  conn.isClosed())
-			   conn = DriverManager.getConnection(mysql_url,
-					mysql_user, mysql_passwd);
-         
-            if (needAuthentication(tokens, conn, req))
+
+			if (conn == null || conn.isClosed())
+				conn = DriverManager.getConnection(mysql_url, mysql_user,
+						mysql_passwd);
+
+			if (needAuthentication(tokens, req))
 				askForPassword(resp);
-		
-			else {			 
-				String sql = getSqlString(tokens, conn);			 
-			 
-				 // if (logger.isDebugEnabled()) 
-				//	  logger .debug("doPost(HttpServletRequest, HttpServletResponse) - InteractionsConnectionImplServlet doPost, sql string is " + sql );  
-			 				
-				PreparedStatement  statement = conn.prepareStatement(sql);
-				 
-				if (methodName.equalsIgnoreCase(GET_PAIRWISE_INTERACTION))  
-				{	String geneId = tokens[2].trim();
-				    statement.setString(1, geneId);
-				    statement.setString(2, geneId);
+
+			else {
+				ResultSet rs = null;				
+				if (methodName.equalsIgnoreCase(GET_PAIRWISE_INTERACTION)) {
+					String geneId = tokens[2].trim();
+					String context = tokens[3].trim();
+					String version = tokens[4].trim();
+					rs = this.getPairWiseInteraction(geneId, context, version);
+				} else if (methodName.equalsIgnoreCase(GET_INTERACTION_TYPES)) {
+					rs = this.getInteractionTypes();
+				} else if (methodName.equalsIgnoreCase(GET_DATASET_NAMES)) {
+					rs = this.getDatasetNames();
+				} else if (methodName.equalsIgnoreCase(GET_VERSION_DESCRIPTOR)) {
+					String context = tokens[2].trim();
+					rs = this.getVersionDescriptor(context);
 				}
-			 
-				ResultSet rs = statement.executeQuery();
-				 
+ 
 				PrintWriter out = resp.getWriter();
 
 				String metaData = new String();
@@ -220,8 +218,8 @@ public class InteractionsConnectionImplServlet extends HttpServlet {
 					}
 					metaData += rsmd.getColumnName(i);
 				}
-				out.println(metaData);                
-		 
+				out.println(metaData);
+
 				// get values
 				while (rs.next()) {
 					String row = new String();
@@ -234,17 +232,15 @@ public class InteractionsConnectionImplServlet extends HttpServlet {
 					}
 					out.println(row);
 				}
-		 
+
 				out.flush();
 				out.close();
-			 
+
 				rs.close();
 				statement.close();
 				resp.setStatus(HttpServletResponse.SC_OK);
-		 
-			}   
-          
-			 
+
+			}
 
 		} catch (IOException e) {
 			try {
@@ -265,12 +261,12 @@ public class InteractionsConnectionImplServlet extends HttpServlet {
 				resp.getWriter().close();
 				logger.error("get SQLException: ", e);
 				isSqlException = true;
-				
+
 			} catch (IOException ioe) {
 				logger
 						.error(
 								"exception in catch block: doPost(HttpServletRequest, HttpServletResponse)", ioe); //$NON-NLS-1$
-			
+
 			}
 		} catch (Throwable e) {
 			try {
@@ -284,24 +280,18 @@ public class InteractionsConnectionImplServlet extends HttpServlet {
 								"exception in catch block: doPost(HttpServletRequest, HttpServletResponse)", ioe); //$NON-NLS-1$
 			}
 
-		}
-		finally
-		{
-			  if ((!methodName.equalsIgnoreCase(GET_PAIRWISE_INTERACTION)) || isSqlException == true)  
-	          {    
-				     try{
-				    	 if (conn != null )
-				    	   conn.close(); 
-				    	 conn = null;
-				     }catch (SQLException e) {}
-				    	 
-				     
-				    
-	                 
-	          }
-		 
-			
-			
+		} finally {
+			if ((!methodName.equalsIgnoreCase(GET_PAIRWISE_INTERACTION))
+					|| isSqlException == true) {
+				try {
+					if (conn != null)
+						conn.close();
+					conn = null;
+				} catch (SQLException e) {
+				}
+
+			}
+
 		}
 
 	}
@@ -313,7 +303,7 @@ public class InteractionsConnectionImplServlet extends HttpServlet {
 
 	}
 
-	private boolean needAuthentication(String[] tokens, Connection conn,
+	private boolean needAuthentication(String[] tokens, 
 			HttpServletRequest req) throws IOException, SQLException {
 		String methodName = tokens[1];
 
@@ -322,7 +312,7 @@ public class InteractionsConnectionImplServlet extends HttpServlet {
 		else {
 			String context = tokens[3];
 			String version = tokens[4];
-			if (getAuthentication(conn, context, version).equalsIgnoreCase("N"))
+			if (getAuthentication(context, version).equalsIgnoreCase("N"))
 				return false;
 			String userInfo = req.getHeader("Authorization");
 			BASE64Decoder decoder = new BASE64Decoder();
@@ -338,35 +328,54 @@ public class InteractionsConnectionImplServlet extends HttpServlet {
 
 		return true;
 	}
-
-	private String getSqlString(String[] tokens, Connection conn)
-			throws SQLException {
+	
+	 
+	
+	private ResultSet getPairWiseInteraction(String geneId, String context, String version) throws SQLException 
+	{
 		String aSql = null;
-		String methodName = tokens[1];
-		if (methodName.equalsIgnoreCase(GET_PAIRWISE_INTERACTION)) {
-			String geneId = tokens[2].trim();
-			String context = tokens[3].trim();
-			String version = tokens[4].trim();
-			int datasetId = getDatasetId(conn, context, version);
-			 
-			aSql = "SELECT pi.ms_id1, pi.ms_id2, pi.gene1, pi.gene2, pi.confidence_value, pi.db1_xref, pi.db2_xref, it.interaction_type FROM pairwise_interaction pi, interaction_dataset ids, interaction_type it";
-			aSql += " WHERE (ms_id1= ?" + " OR ms_id2= ?" ;
-			aSql += ") AND pi.interaction_type=it.id AND pi.id=ids.interaction_id And ids.dataset_id="
-					+ datasetId;		
-		} else if (methodName.equalsIgnoreCase(GET_INTERACTION_TYPES)) {
-			aSql = "SELECT  interaction_type FROM  interaction_type";
-		} else if (methodName.equalsIgnoreCase(GET_DATASET_NAMES)) {
-			aSql = "SELECT DISTINCT name FROM dataset";
-		} else if (methodName.equalsIgnoreCase(GET_VERSION_DESCRIPTOR)) {
-			String context = tokens[2].trim();
-			aSql = "SELECT version, authentication_yn FROM dataset where name='"
-					+ context + "'";
-		}
-
-		return aSql;
+		int datasetId = getDatasetId(conn, context, version);
+		aSql = "SELECT pi.ms_id1, pi.ms_id2, pi.gene1, pi.gene2, pi.confidence_value, pi.db1_xref, pi.db2_xref, it.interaction_type FROM pairwise_interaction pi, interaction_dataset ids, interaction_type it";
+		aSql += " WHERE (ms_id1= ?" + " OR ms_id2= ?";
+		aSql += ") AND pi.interaction_type=it.id AND pi.id=ids.interaction_id And ids.dataset_id="
+				+ datasetId;
+	    statement = conn.prepareStatement(aSql);
+		statement.setString(1, geneId);
+		statement.setString(2, geneId);
+		ResultSet rs = statement.executeQuery();
+		 
+		return rs;
 	}
+	
+	private ResultSet getInteractionTypes() throws SQLException 
+	{
+		String aSql = null;
+		aSql = "SELECT  interaction_type FROM  interaction_type";
+		statement = conn.prepareStatement(aSql);		 
+		ResultSet rs = statement.executeQuery();
+		return rs;
+	}
+	
+	private ResultSet getDatasetNames() throws SQLException 
+	{
+		String aSql = null;
+		aSql = "SELECT DISTINCT name FROM dataset";
+		statement = conn.prepareStatement(aSql);		 
+		ResultSet rs = statement.executeQuery();
+		return rs;
+	}
+	
+	private ResultSet getVersionDescriptor(String context) throws SQLException 
+	{
+		String aSql = null;
+		aSql = "SELECT version, authentication_yn FROM dataset where name='"
+			+ context + "'";
+		statement = conn.prepareStatement(aSql);		 
+		ResultSet rs = statement.executeQuery();
+		return rs;
+	}	 
 
-	private String getAuthentication(Connection conn, String context,
+	private String getAuthentication(String context,
 			String version) throws SQLException {
 		Statement statement = conn.createStatement();
 		String value = "N";
